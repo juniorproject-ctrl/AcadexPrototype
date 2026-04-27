734825bc286c3d22221e81b97e4054c5704a5470
juniorproject <juniorprojecteccp@gmail.com>
2026-04-27 04:16:12 +0400

    Add files via upload

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -7,9 +7,9 @@ type BrandLogoProps = {
 };
 
 const sizeClasses = {
-  sm: 'h-14',
-  md: 'h-16',
-  lg: 'h-24',
+  sm: 'h-24',
+  md: 'h-28',
+  lg: 'h-32',
 };
 
 export default function BrandLogo({ size = 'md', linked = false, className = '' }: BrandLogoProps) {
