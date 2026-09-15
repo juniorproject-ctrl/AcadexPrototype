a51622675c881161f160ddaa80fdf6aa29736c17
juniorproject <juniorprojecteccp@gmail.com>
2026-09-15 22:53:58 +0400

    server.ts

--- server.ts
+++ server.ts
@@ -62,7 +62,7 @@ async function buildTransporter() {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.use(cors());
   app.use(express.json({ limit: '10mb' }));
